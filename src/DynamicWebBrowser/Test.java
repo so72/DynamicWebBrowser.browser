3dec17e298773a1251a15187f24925261a4ff1f3
Steffen Olson <sknoslo@gmail.com>
2013-04-07 10:57:26 -0700

    Added java.net.URI usage to Test

--- src/DynamicWebBrowser/Test.java
+++ src/DynamicWebBrowser/Test.java
@@ -4,6 +4,8 @@ import DynamicWebBrowser.protocols.Protocol;
 import DynamicWebBrowser.protocols.ProtocolFinder;
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
@@ -13,15 +15,20 @@ public class Test {
     
     public static void main(String args[]) {
         ProtocolFinder finder = new ProtocolFinder();
+        URI uri = null;
+        try {
+            String uriString = "time:";
+            if (uriString.endsWith(":")) uriString += "/";
+            uri = new URI(uriString);
+        } catch (URISyntaxException ex) {
+            System.out.println("URI syntax");
+            System.exit(1);
+        }
         
         for (int i = 0; i < 2; i++) {
-            Protocol time = finder.findProtocol("time");
+            Protocol time = finder.findProtocol(uri.getScheme());
             if (time != null) {
-                try {
-                    System.out.println(time.execute(new URI("time")));
-                } catch (URISyntaxException ex) {
-                    System.out.println("Invalid URI");
-                }
+                System.out.println(time.execute(uri));
             } else {
                 System.out.println("Protocol not implemented");
             }
